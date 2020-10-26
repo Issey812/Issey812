687958b60d2b5ddb8f5c914930c33b46d47da81a
Issey812 <shino20011021@gmail.com>
2020-10-26 22:37:55 +0900

    課題提出 - Javaの基礎

---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		/* hogehoge2が｝の外側にあり、そのまま実行するとエラーが出たため、
+		 * Task1と同様のコード配置にした。
+		 */
+		
+		System.out.println("hogehoge");
+		System.out.print("hogehoge2");
+
+	}
+	
+}
