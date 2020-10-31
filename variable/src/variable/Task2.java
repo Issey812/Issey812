82160aa03936f83e24b8503ccebf2b43df34853c
Issey812 <shino20011021@gmail.com>
2020-10-31 22:26:56 +0900

    課題提出 - 変数・定数

---
+++ variable/src/variable/Task2.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+			String name = "篠崎　一生";
+			final String TELEPHONE_NUMBER = "090-1234-5678";
+			
+			System.out.println( name + "です。");
+			System.out.println("私の電話番号は" + "\'" + TELEPHONE_NUMBER + "\'" + "です。");
+			
+
+	}
+
+}
